dbf841587489e05c5263c8e925400a4085c068f5
espeniv <espeiv@stud.ntnu.no>
2024-10-08 15:24:40 +0200

    Completed Task 3 ab

--- Delivery1/Main.scala
+++ Delivery1/Main.scala
@@ -77,5 +77,35 @@ object Main extends App {
     with the main difference being the syntax used to implement it. */
 
 
+
     //Task 3a
+    def createThreadWithFunction(function: () => Unit): Thread = {
+        //Runnable makes it possible to run custom behavior in the thread, such as a function
+        val thread = new Thread(new Runnable {
+            def run() = {
+                //The function that is passed into this function is called here when the thread is started
+                function()
+            }
+        })
+        return thread
+    }
+
+    val simpleFunction = () => println("Function has been called")
+    val thread = createThreadWithFunction(simpleFunction)
+    println("Thread created, but not started yet")
+    //Thread is started -> function is called with thread.start()
+    thread.start()
+
+
+    //Task 3b
+    /* The code is supposed to increment the value of value1 while decreasing the value of value2, and then update the sum of the two values. This should happen until the value of value1 is 0,
+    and at this point the values should be reset to 1000 and 0 respectively. The state of these values, in addition the sum, is logged to the console continously. The sum of the values should always be 1000.
+    This is not working as expected, as there are errors with this code that leads to the state of the values being incosistent and the sum not being 1000. This is likely happening because there is no form 
+    of synchronization between the two threads, this can for example lead to the threads reading from or writing to a value at the same time etc., which leads to invalidating eachothers read information and updates. 
+    I assume that it would be possible to experience the same behavior in Oz, as concurrent threads can be created just as loosely as the ones we have in this example. I think it therefore in both cases important
+    to use some form of synchronization to avoid these issues, regardless of the implementation language. Synchronization issues like these would be very fatal in a system like a banking system (like the one 
+    in this scala project), as if a bank account has both an incoming and outgoing transaction at the same time without synchronization it could lead to the balance of the bank account being updated incorrectly.*/
+    
+    
+    //Task 3c - See seperate file
 }
